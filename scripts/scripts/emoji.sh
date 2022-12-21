5508544a546167b66e383082892ebb09ca0763a4
sadwhale09 <sadwhale09@github.com>
2022-12-21 16:20:20 +0100

    Change emoji picker to show in rofi

--- scripts/scripts/emoji.sh
+++ scripts/scripts/emoji.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
-grep -v "#" ~/scripts/unicode | dmenu -i -l 10 -fn Monospace-18 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+# grep -v "#" ~/scripts/unicode | dmenu -i -l 10 -fn Monospace-18 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+
+# Show picker in rofi instead
+grep -v "#" ~/scripts/unicode | rofi -dmenu -i -l 10 -fn Monospace-18 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
 
 pgrep -x dunst >/dev/null && notify-send "$(xclip -o -selection clipboard) copied to clipboard."
