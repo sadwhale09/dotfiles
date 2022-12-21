61fcc154326ed3b2e7c0ec77096a836f344e4643
sadwhale09 <sadwhale09@github.com>
2022-12-21 16:12:23 +0100

    Add scripts folder

---
+++ scripts/scripts/emoji.sh
@@ -0,0 +1,5 @@
+#/bin/sh
+
+grep -v "#" ~/scripts/unicode | dmenu -i -l 10 -fn Monospace-18 | awk '{print $1}' | tr -d '\n' | xclip -selection clipboard
+
+pgrep -x dunst >/dev/null && notify-send "$(xclip -o -selection clipboard) copied to clipboard."
