b183eef33c7844b2b50de3cb40d1c25ac4c7254f
sadwhale09 <sadwhale09@github.com>
2023-03-08 23:04:50 +0100

    Update display-picker.sh to be conditional

--- scripts/scripts/display-picker.sh
+++ scripts/scripts/display-picker.sh
@@ -6,3 +6,8 @@
 xrandr --output VGA1 --right-of LVDS1 --auto
 # grep -v "#" ~/scripts/displays | rofi -dmenu | tr -d '\n' | xrandr
 
+if [[xrandr -q | grep "VGA1 connected"]]; then
+  xrandr --output VGA1 --right-of LVDS1 --auto
+else
+  xrandr --auto
+fi
