9e014701cbd80b4cc6f16e526c788e9c30a5c2c3
sadwhale09 <sadwhale09@github.com>
2023-03-29 21:45:44 +0200

    Edit state full

--- scripts/scripts/battery-percentage.sh
+++ scripts/scripts/battery-percentage.sh
@@ -4,14 +4,14 @@ power=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage
 state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | sed -e 's/ //g')  # Apparently `tr -d` deletes all occurences of symbols in " "
 current=$(($power+0))
 low=25
-full=100
+# full=100
 
 if [ "$current" -lt "$low" ]; then  # -lt == less than. Opposite is -gt
   if [ "$state" == "state:discharging" ]; then
     notify-send -u critical "Plug the computer"; mpv ./sounds/anon-odkurz-salon.mp3
   fi
   # elif [ "$current" = "$full" ] #|| [ "$current" -gt "$full" ]
-elif [ "$state" = "full" ]; then #|| [ "$current" -gt "$full" ]
+elif [ "$state" = "state:full" ]; then #|| [ "$current" -gt "$full" ]
   notify-send "Battery full"
 fi
 
