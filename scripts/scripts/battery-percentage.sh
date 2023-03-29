0f6314afa652e50bfc2a6d992ddc451fc3bb9a8f
sadwhale09 <sadwhale09@github.com>
2023-03-29 13:40:05 +0200

    Notification appears accordingly to battery state

--- scripts/scripts/battery-percentage.sh
+++ scripts/scripts/battery-percentage.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 # Requires crontab -e "*/5 * * * * ~/scripts/battery-percentage.sh"
 power=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | tr -d "percentage:" | tr -d "%" | sed -e 's/ //g')
+state=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep state | sed -e 's/ //g')  # Apparently `tr -d` deletes all occurences of symbols in " "
 current=$(($power+0))
-low=20
+low=25
 full=100
 
-if [ "$current" -lt "$low" ]  # -lt == less than. Opposite is -gt
-then
-  notify-send "Plug the computer"
-elif [ "$current" = "$full" ] #|| [ "$current" -gt "$full" ]
-then
+if [ "$current" -lt "$low" ]; then  # -lt == less than. Opposite is -gt
+  if [ "$state" == "state:discharging" ]; then
+    notify-send -u critical "Plug the computer"; mpv ./sounds/anon-odkurz-salon.mp3
+  fi
+  # elif [ "$current" = "$full" ] #|| [ "$current" -gt "$full" ]
+elif [ "$state" = "full" ]; then #|| [ "$current" -gt "$full" ]
   notify-send "Battery full"
 fi
 
-
+# TODO Use binary operators instead of if statements
+# [ "$state" == "discharging" ] && notify-send -u critical "Plug the computer"
+# [ "$current" -lt "$low" ] && notify-send -u critical "Plug the computer"
