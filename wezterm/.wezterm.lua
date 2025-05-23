988d7dec5db009dbe5eace97605d2732ef03148c
sadwhale09 <51019837+sadwhale09@users.noreply.github.com>
2025-05-23 17:28:08 +0200

    Update margin in wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -6,8 +6,8 @@ config.color_scheme = "Tokyo Night"
 config.scrollback_lines = 5000
 config.hide_tab_bar_if_only_one_tab = true
 config.window_padding = {
-	left = 0,
-	right = 0,
+	left = "0.5cell",
+	right = "0.5cell",
 	top = 0,
 	bottom = 0, -- Now, tmux status bar will look gucci
 }
